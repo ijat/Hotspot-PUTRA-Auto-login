f2f2f1ceda1aff9446f0de90808136c48341dde0
Syafiq Izzat Hussin <Syafiq Izzat Hussin>
2016-08-13 18:43:48 +0800

    Bug fix

--- Source/hotspot.py
+++ Source/hotspot.py
@@ -44,7 +44,7 @@ def network_check():
 
 def check_upm_wifi():
     if not check_connectivity("https://authenticate.upm.my/"):
-        print("You're not connected to Hotspot@UPM")
+        print("You're not connected to Hotspot@PUTRA")
         return False
     else:
         return True
@@ -54,13 +54,13 @@ def main():
     try:
         user_data['username'] = input("UPM ID: ")
         user_data['password'] = getpass.getpass(prompt='Password: ')
-        print("\n === Press CTRL+C to exit ===\n")
+        print("\n === Press CTRL+C to exit (2 times to force exit) ===\n")
     except:
         print("Invalid input")
         exit(100)
     try:
         while (not check_upm_wifi()):
-            print("Detecting Hotspot@UPM in 5 seconds...")
+            print("Retrying in 5 seconds...")
             time.sleep(5)
         login()
         if check_connectivity("https://ijat.my/"):
@@ -81,11 +81,11 @@ def main():
 
 if __name__ == '__main__':
     def signal_handler(signal, frame):
-        print('\n-Bye-')
-        sys.exit(0)
+        print('* Exiting...')
+        exit()
     signal.signal(signal.SIGINT, signal_handler)
     print("\nHotspot@Putra Auto-Login v0.1\n\tby Ijat.my\n")
     try:
         main()
     except:
-        print("\nSomething went wrong")
+        exit(1)
